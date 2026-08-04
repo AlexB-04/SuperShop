2a826fe5cc553f55c08710eb34e6e9e793c441cd
AlexB-04 <bezverkhy04@gmail.com>
2026-08-04 12:38:36 +0100

    Modal windows

--- SuperShop/Data/Entities/Order.cs
+++ SuperShop/Data/Entities/Order.cs
@@ -34,7 +34,7 @@ namespace SuperShop.Data.Entities
         public decimal Value => Items == null ? 0 : Items.Sum(i => i.Value);
 
         [Display(Name = "Order date")]
-        [DisplayFormat(DataFormatString = "{0:mm/dd/yyyy HH:mm tt}", ApplyFormatInEditMode = false)]
+        [DisplayFormat(DataFormatString = "{0:MM/dd/yyyy HH:mm tt}", ApplyFormatInEditMode = false)]
         public DateTime? OrderDateLocal => this.OrderDate == null ? null : this.OrderDate.ToLocalTime();
     }
 }
